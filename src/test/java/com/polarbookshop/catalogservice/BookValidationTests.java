ef01dc2b82327b307ded11270445943f72b0ff41
James Stocco, III <james.stocco@bwpackagingsystems.com>
2023-07-06 08:26:51 -0500

    Chapter 3 - Initial Commit

--- src/test/java/com/polarbookshop/catalogservice/BookValidationTests.java
+++ src/test/java/com/polarbookshop/catalogservice/BookValidationTests.java
@@ -1,10 +1,16 @@
-package com.polarbookshop.catalogservice.domain;
+package com.polarbookshop.catalogservice;
 
+import com.polarbookshop.catalogservice.domain.Book;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
+import javax.validation.ConstraintViolation;
 import javax.validation.Validation;
 import javax.validation.Validator;
 import javax.validation.ValidatorFactory;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class BookValidationTests {
 
@@ -14,6 +20,21 @@ public class BookValidationTests {
     static void setUp() {
         ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
         validator = factory.getValidator();
+    }
+
+    @Test
+    void whenAllFieldsCorrectThanValidationSucceeds() {
+        var book = new Book("1234567890", "Title", "Author", 9.90);
+        Set<ConstraintViolation<Book>> violations = validator.validate(book);
+        assertThat(violations).isEmpty();
+    }
+
+    @Test
+    void whenIsbnDefinedButIncorrectThenValidationFails() {
+        var book = new Book("a234567890", "Title", "Author", 9.90);
+        Set<ConstraintViolation<Book>> violations = validator.validate(book);
+        assertThat(violations).hasSize(1);
+        assertThat(violations.iterator().next().getMessage()).isEqualTo("The ISBN format must be valid.");
     }
 
 }
